16da106a455c22b3673dc410cb5ce735247932bc
Basith P <basithpofficial@gmail.com>
2023-11-05 23:07:50 +0530

    feat: create post schema

---
+++ models/post.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+    },
+    content: {
+      type: String,
+      required: [true, "Content is required."],
+    },
+    imageUrl: {
+      type: String,
+      required: [true, "Image URL is required."],
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Creator is required."],
+    },
+  },
+  {
+    timestamps: true,
+    //   toJSON: {
+    //     transform: (doc, ret) => {
+    //       ret.id = doc._id;
+    //       delete ret._id;
+    //       delete ret.__v;
+    //       return ret;
+    //     },
+    //   },
+  }
+);
+
+module.exports = mongoose.model("Post", postSchema);
