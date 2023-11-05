a3fca40876772b9dc3ae44289d6771716e7f9fef
Basith P <basithpofficial@gmail.com>
2023-11-05 23:07:42 +0530

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+const port = process.env.PORT || 8080;
 
+/// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(cors());
 
-// cors
-
+/// Routes
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB."))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => console.log(`Listening on port ${port}.`));
