a450f9ecd66eddc9b425a9ef1b801b7a3ecc5e62
Basith P <basithpofficial@gmail.com>
2023-11-07 17:41:24 +0530

    feat: add error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -13,8 +15,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+/// Static Files
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 /// Routes
 app.use("/feed", feedRoutes);
+
+/// Error Handling Middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  const { statusCode, message, data } = error;
+  res.status(statusCode || 500).json({ message, data });
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
--- controllers/feed.js
+++ controllers/feed.js
@@ -18,10 +18,10 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "Please check your inputs!",
-      errors: errors.array(),
-    });
+    const error = new Error("Validation failed.");
+    error.statusCode = 422;
+    // error.data = errors.array();
+    throw error;
   }
 
   const { title, content } = req.body;
@@ -42,6 +42,7 @@ exports.createPost = (req, res, next) => {
       });
     })
     .catch((err) => {
-      console.log(err);
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
     });
 };
